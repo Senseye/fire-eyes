ba05ebb06c61823e52139e70414c4133b87bce5c
Sergiu <oalasergiu@gmail.com>
2019-03-10 17:21:14 +0100

    Updates

---
+++ precache-manifest.b9147aeea3eb28d54aa9ef9a6952c52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fef67f535833ca4f04e9",
+    "url": "/Senseye/fire-eyes/static/js/runtime~main.fef67f53.js"
+  },
+  {
+    "revision": "fd0a13fb5ba5b905a34b",
+    "url": "/Senseye/fire-eyes/static/js/main.fd0a13fb.chunk.js"
+  },
+  {
+    "revision": "4e12b0f714ea694af3b8",
+    "url": "/Senseye/fire-eyes/static/js/1.4e12b0f7.chunk.js"
+  },
+  {
+    "revision": "fd0a13fb5ba5b905a34b",
+    "url": "/Senseye/fire-eyes/static/css/main.9c0e2325.chunk.css"
+  },
+  {
+    "revision": "5216fb2f823dc6318ab334962854bd14",
+    "url": "/Senseye/fire-eyes/index.html"
+  }
+];
